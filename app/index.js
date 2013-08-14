5f009a1746494476d15f501f8494046e25895f86
jarvis <kinua1230@gmail.com>
2013-08-14 15:54:21 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var path = require('path');
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+exports = module.exports = function(args, options) {
+  yeoman.generators.Base.apply(this, arguments);
+  this.basename = path.basename(options.env.cwd);
+  this.on('end', function() {
+    this.installDependencies({
+      skipInstall: options['skip-install']
+    });
+  });
+}
+
+util.inherits(exports, yeoman.generators.Base);
+
+exports.prototype.askFor = function() {
+  var done = this.async();
+  console.log(this.yeoman);
+
+  this.prompt([
+    {
+      name: 'name',
+      message: 'Project name',
+      "default": this.basename
+    }, {
+      name: 'description',
+      message: 'Description',
+      "default": ''
+    }, {
+      name: 'version',
+      message: 'Version',
+      "default": '0.0.0'
+    }, {
+      name: 'author',
+      message: 'Author',
+      "default": this.user.git.username
+    }, {
+      name: 'email',
+      message: 'Email',
+      "default": this.user.git.email
+    }, {
+      name: 'usingGithub',
+      message: 'Using GitHub?',
+      type: 'confirm',
+      "default": true
+    }, {
+      name: 'githubUser',
+      message: 'GitHub user',
+      "default": function(answers) {
+        var githubUser = this.shell.exec('git config --get github.user', {silent: true}).output.trim();
+        return githubUser || answers.author;
+      }.bind(this),
+      when: function(answers) {
+        return answers.usingGithub;
+      }
+    }, {
+      name: 'githubRepo',
+      message: 'GitHub repo',
+      "default": function(answers) {
+        return answers.name;
+      }.bind(this),
+      when: function(answers) {
+        return answers.usingGithub;
+      }
+    }, {
+      name: 'licenses',
+      message: 'Licenses',
+      type: 'checkbox',
+      choices: [{name: 'MIT', checked: true}, 'Apache-2.0', 'MPL-2.0', 'GPL-2.0'],
+      "default": 'MIT',
+      when: function(answers) {
+        return answers.usingGithub;
+      }
+    }
+  ], function(props) {
+    this._.extend(this, props);
+    done();
+  }.bind(this));
+};
+
+exports.prototype.copyRootFile = function() {
+  this.directory('root', '.');
+};
+
+exports.prototype.copyLicenses = function() {
+  if (!this.licenses || this._.isEmpty(this.licenses)) {
+    return;
+  };
+  this._.each(this.licenses, function(license) {
+    var licenseFile = 'LICENSE-' + license;
+    this.template('licenses/' + licenseFile, licenseFile);
+  }.bind(this));
+};
+
+exports.prototype.copyTemplateFiles = function() {
+  this.template('_README.md', 'README.md');
+  this.template("_package.json", 'package.json');
+};
---
+++ app/templates/_README.md
@@ -0,0 +1,17 @@
+# <%= name %>
+
+> <%= description %>
+
+## Getting Started<% if (usingGithub) { %>
+
+```sh
+$ npm install -g <%= githubUser %>/<%= githubRepo %>
+$ mkdir /path/to/your/project
+$ cd $_
+$ yo <%= githubRepo.replace(/^generator-/, '') %>
+```<% } else { %>
+_(Coming soon)_<% } %><% if (usingGithub && licenses && licenses.length) { %>
+
+## License
+
+Copyright (c) <%= new Date().getFullYear() %> <%= author %> Licensed under the <%= licenses.join(', ') %> license<%= licenses.length === 1 ? '' : 's' %>.<% } %>
